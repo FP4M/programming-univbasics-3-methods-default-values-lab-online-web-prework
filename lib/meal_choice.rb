97be8802af8708ead46571017ce41a1635250c8a
Prince Charles <princecharles35@yahoo.com>
2019-09-23 16:22:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 meal_choice
-meal_choice_veg1
-meal_choice_veg2
-meal_choice_protein 'meat.'
+meal_choice_veg1 'A plate of #{protein}'
+meal_choice_veg2 'so that you can also see what you've order'
+meal_choice_protein 'meat'
